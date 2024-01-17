889bad5d7500d8ae170e101bd65a85633868f20d
RAULCRESCENCIO <116508614+RAULCRESCENCIO@users.noreply.github.com>
2024-01-16 20:09:49 -0600

    Actividad 3 calculadora

---
+++ Actividad 3/calculadora.js
@@ -0,0 +1,27 @@
+// Función de suma
+const suma = (a, b) => a + b;
+
+// Función de resta
+const resta = (a, b) => a - b;
+
+// Función de multiplicación
+const multiplicacion = (a, b) => a * b;
+
+// Función de división
+const division = (a, b) => a / b;
+
+// Función de x al cuadrado
+const cuadrado = x => x ** 2;
+
+// Función de raíz cuadrada de x
+const raizCuadrada = x => Math.sqrt(x);
+
+// Función de 1/x
+const inverso = x => 1 / x;
+
+// Función de residuo
+const residuo = (a, b) => a % b;
+
+export{suma,resta,multiplicacion,division,cuadrado,raizCuadrada,inverso,residuo}
+
+
